5c6ca372affd0ab330d21e2a13786e19698f24d5
Jason Serafin <jtserafin@gmail.com>
2022-12-12 16:24:27 -0500

    Operational Commit

---
+++ opc-da-client.js
@@ -0,0 +1,43 @@
+const OPC = require('st-opc-da')
+
+module.exports = function(RED) {
+    function OpcClient(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        const opc = new OPC()
+
+        opc.on('tag change', tag => {
+
+            let msg = {payload: tag}
+            node.send(msg)
+        })
+
+        node.on('input', function(msg) {
+            
+            if(msg.payload.cmd === 'list servers') {
+                opc.getOPCServers().then(data => {
+                    msg.payload = data;
+                    node.send(msg)
+                })
+            }
+
+            if(msg.payload.cmd === 'add item') {
+                opc.addReadOPCTag(config.server, msg.payload.data.name)
+            }
+
+            if(msg.payload.cmd === 'start server') {
+                opc.startReadServer(config.server, config.update)
+            }
+
+            if(msg.payload.cmd === 'get values') {
+                msg.payload = opc.tags
+                node.send(msg)
+            }
+
+        });
+
+    }
+
+    RED.nodes.registerType("opc-da-client",OpcClient);
+}
